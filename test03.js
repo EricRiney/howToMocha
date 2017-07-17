e81b62817595ecb10ab96daeae831f20d706efd9
EricRiney <fails.to.install@gmail.com>
2017-07-16 18:28:02 -0700

    tutorial finished

--- test03.js
+++ test03.js
@@ -1,19 +1,21 @@
-var assert = require('assert');
-var expected, current;
-before(function(){
-  expected = ['a', 'b', 'c'];
-})
-describe('String#split', function(){
-  beforeEach(function(){
-   current = 'a,b,c'.split(',');
+    var expect = require('expect.js');
+    var expected, current;
+    before(function(){
+      expected = ['a', 'b', 'c'];
+    })
+    describe('String#split', function(){
+      beforeEach(function(){
+        current = 'a,b,c'.split(',');
+        console.log(current);
+      })
+      it('should return an array', function(){
+        expect(Array.isArray(current)).to.be.true;
+      });
+
+    it('should return the same array', function(){
+      expect(expected.length).to.equal(current.length);
+      for (var i=0; i<expected.length; i++) {
+        expect(expected[i]).equal(current[i]);
+      }
+    })
   })
-  it('should return an array', function(){
-    assert(Array.isArray(current));
-  });
-  it('should return the same array', function(){
-    assert.equal(expected.length, current.length, 'arrays have equal length');
-    for (var i=0; i<expected.length; i++) {
-      assert.equal(expected[i], current[i], i + 'element is equal');
-    }
-  })
-})
